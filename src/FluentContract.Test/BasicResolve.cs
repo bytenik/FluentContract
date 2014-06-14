bd653fa994ee7b46d115f3cc997992f54f8eb0a0
David Pfeffer <bytenik@bytenik.com>
2014-06-14 09:54:36 -0400

    added failing test for resolving null

---
+++ src/FluentContract.Test/BasicResolve.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FluentContract.Test
+{
+    public class BasicResolve
+    {
+        [Fact]
+        public void Can_Resolve_Null()
+        {
+            var mappings = new FluentMappings();
+            mappings.ContractResolver.ResolveContract(null);
+        }
+    }
+}
